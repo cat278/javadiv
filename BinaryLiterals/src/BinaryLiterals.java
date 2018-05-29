5ea73b2540e6f63063e87e9c3555b3f3e9b8e8d3
divyag <divyag@10.11.2.149>
2018-05-29 18:54:38 +0530

    Binary Literals commit

---
+++ BinaryLiterals/src/BinaryLiterals.java
@@ -0,0 +1,17 @@
+//Package BinaryLiterals;
+
+public class BinaryLiterals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=5;
+		System.out.println(i);
+		int k=101;
+		System.out.println(k);
+		int j=0B101;
+		System.out.println(j);
+		int l=0b1000000;
+		System.out.println(l);
+	}
+
+}
