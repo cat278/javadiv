527fdfb09493efeb726fab43af918a0a6fffdc5e
divyag <divyag@192.168.43.160>
2018-05-31 08:21:18 +0530

    SwitchWithStrings commit

---
+++ SwitchWithStirng/src/SwitchWithString.java
@@ -0,0 +1,18 @@
+
+public class SwitchWithString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s ="hello" ; // Hello // three // Hi
+switch (s)
+{
+case "Hi" : System.out.println("Hi");
+			break;
+case "hello" : System.out.println("Hello");
+ 			break;
+default : System.out.println("blah blah..");
+ 			
+}
+	}
+
+}
