83da2d6ef6b3fa419d7d434e2d52819184fae785
divyag <divyag@192.168.43.160>
2018-05-31 08:09:20 +0530

    IfElse commit

---
+++ IfElse/src/IfElse.java
@@ -0,0 +1,13 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=8; //5;
+if (i%2 == 0) {
+	System.out.println("even");}
+else
+	System.out.println("odd");
+	}
+
+}
