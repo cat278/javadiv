dcb564ac7ca3abf56b36b6977c7a89ecadcf8e21
divyag <divyag@192.168.43.160>
2018-05-31 08:14:03 +0530

    LeftRight commit

---
+++ LeftRight/src/LeftRight.java
@@ -0,0 +1,16 @@
+
+public class LeftRight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a =8;
+ int b= a>>2;
+ System.out.println(b);
+ int c= a << 2;
+ System.out.println(c);
+ int d=25;
+ int e = d >>2;
+ System.out.println(e);
+	}
+
+}
