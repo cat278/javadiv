c804c8818dbf16f263f0d70d8dee775b39b5964a
divyag <divyag@192.168.43.160>
2018-05-31 08:20:10 +0530

    Switch commit

---
+++ Switch/src/Switch.java
@@ -0,0 +1,23 @@
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=6;//9
+		
+		switch (i)
+		{
+		case 0 : System.out.println("Zero");
+				break;
+		case 1 : System.out.println("One");
+				break;
+		case 7:System.out.println("Seven");
+				break;
+		case 9 :System.out.println(" Nine");
+				break;
+		default : System.out.println("Some Number");
+		}
+		
+	}
+
+}
