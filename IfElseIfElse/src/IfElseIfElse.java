a0105aacb0da2ad2148628807f2d80e67608a3a4
divyag <divyag@192.168.43.160>
2018-05-31 08:10:33 +0530

    IfElseIfElse commit

---
+++ IfElseIfElse/src/IfElseIfElse.java
@@ -0,0 +1,18 @@
+
+public class IfElseIfElse {
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		a =10;//4;
+		b=5;
+		c=3;
+		
+		if (a>b && a>c)
+			System.out.println(a);
+		else if (b>c)
+		    System.out.println(b);
+		else 
+			System.out.println(c);
+	}
+
+}
