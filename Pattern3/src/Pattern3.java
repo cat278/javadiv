6e18f15699870c2230923627de33fb05c2b38ddb
divyag <divyag@192.168.43.160>
2018-05-31 08:17:51 +0530

    Pattern3 commit

---
+++ Pattern3/src/Pattern3.java
@@ -0,0 +1,28 @@
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			for(int i=1; i<=4; i++) 
+			{
+			for (int j=1;j<=4;j++)
+		      {
+				//System.out.printf("* ");
+				if(j==2||i==2||j==3||i==3)
+				System.out.printf("* ");
+
+				else 
+					System.out.printf("  ");//2
+				}
+				 
+			System.out.printf(" \n");
+			}
+			}
+
+		}
+
+
+	
+
+
