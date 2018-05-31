b10eef14f301c41cb548aed2787efaa3020a8a94
divyag <divyag@192.168.43.160>
2018-05-31 08:19:11 +0530

    PostPre commit

---
+++ PostPre/src/PostPre.java
@@ -0,0 +1,26 @@
+
+public class PostPre {
+
+	public static void main(String[] args) {
+		
+		int i=5;
+		System.out.println(i);
+		i ++;
+		int j=i;
+		System.out.println(j);
+		int k =8;
+		k--;
+		int l= k;
+		System.out.println(l);
+		int a =4;
+		++a;
+		
+		System.out.println(a);
+		int c =6;
+		--c;
+		System.out.println(c);
+		
+
+	}
+
+}
