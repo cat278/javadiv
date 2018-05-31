1514a02b867d1792e0b07259b80dd37d33090e2e
divyag <divyag@192.168.43.160>
2018-05-31 08:05:31 +0530

    DoWhile commit

---
+++ DoWhile/src/DoWhile.java
@@ -0,0 +1,13 @@
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=15;
+do {
+	System.out.println(i);
+	i++ ;
+} while(i<=20);
+	}
+
+}
