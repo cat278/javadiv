9859382329ca30442cc5df08d4ab18325c8afa28
divyag <divyag@192.168.43.160>
2018-05-31 08:22:02 +0530

    Terinary commit

---
+++ Terinary/src/Terinary.java
@@ -0,0 +1,20 @@
+
+public class Terinary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=14;
+int j=5;
+
+j=i== 14? 7:100;
+ 
+//if (i==j)
+//	j=7;
+//else 
+//	j=10;
+
+System.out.println(j);
+	}
+	
+
+}
