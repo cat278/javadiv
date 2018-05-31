3e69421655c948b81bd9507a837f1374cf780739
divyag <divyag@192.168.43.160>
2018-05-31 08:14:49 +0530

    LetSee commit

---
+++ LetSee/src/LetSee.java
@@ -0,0 +1,22 @@
+
+public class LetSee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Object obj1;
+Object obj2 ;
+obj2 =true ? new Integer(10): new Double(15.00);
+//if (true)
+//{
+//	obj1 = new Integer(10);
+//	}
+//else
+//{
+//	obj1= new Double(20.0);
+//	}
+
+System.out.println(obj2);
+
+	}
+
+}
