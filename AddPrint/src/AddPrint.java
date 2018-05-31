361d06e05b14644d34193a64419d3848975f3c0d
divyag <divyag@192.168.43.160>
2018-05-31 08:28:04 +0530

    AddPrint commit

---
+++ AddPrint/src/AddPrint.java
@@ -0,0 +1,18 @@
+\\
+public class AddPrint {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+int i, j , k;
+i=10;
+j=11;
+
+k= i+j;
+
+System.out.printf ("the addition of %d and %d is %d ", i,j,k);
+
+
+	}
+
+}
