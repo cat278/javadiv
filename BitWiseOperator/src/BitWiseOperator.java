09d7337abb048ad7a21066ff3874e71bbe137426
divyag <divyag@192.168.43.160>
2018-05-31 08:02:04 +0530

    Bitwise commit

---
+++ BitWiseOperator/src/BitWiseOperator.java
@@ -0,0 +1,12 @@
+
+public class BitWiseOperator {
+public static void main(String[] args) {
+	int a =25;
+	int b=15;
+	int c=a & b ;
+	System.out.println(c);
+	int d = a |b ;
+	System.out.println(d);
+}
+
+}
