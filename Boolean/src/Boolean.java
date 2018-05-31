443fb41a8fd6a13e4abac0fb0425530b3e9a35ae
divyag <divyag@192.168.43.160>
2018-05-31 08:03:57 +0530

    Boolean commit.

---
+++ Boolean/src/Boolean.java
@@ -0,0 +1,13 @@
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+boolean b= true;
+System.out.println(b);
+b=!b;
+System.out.println(b);
+	}
+
+}
