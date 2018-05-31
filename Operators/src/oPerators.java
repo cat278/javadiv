f70909d0d338e8fbe6623370f608ec60e2dd8848
divyag <divyag@192.168.43.160>
2018-05-31 08:16:22 +0530

    Operator commit

---
+++ Operators/src/oPerators.java
@@ -0,0 +1,18 @@
+
+public class oPerators {
+public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+	
+	int i=6;
+	
+// 	i++;
+	
+	//System.out.println(i);
+	
+	i = i++;
+	
+	System.out.println(i);
+}
+
+}
