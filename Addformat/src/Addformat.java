79365558ff7cf2c8f788b67d38f1be773d774378
divyag <divyag@192.168.43.160>
2018-05-31 08:27:18 +0530

    AddFormat commit

---
+++ Addformat/src/Addformat.java
@@ -0,0 +1,14 @@
+
+public class Addformat {
+
+	public static void main(String[] args) {
+		int i=4;
+		int j=5;
+		int k= i+j;
+		
+		System.out.println("the addlition of " + i + "and" + j + "is" +k) ;
+		// TODO Auto-generated method stub
+
+	}
+
+}
